4df146d9ad9c571db91ee6c90cc07731aa910b12
Trey <73353716+TreyWW@users.noreply.github.com>
2025-02-17 22:07:26 +0000

    Fixed cloudfront

--- loading-instance.tf
+++ loading-instance.tf
@@ -6,17 +6,55 @@ resource "aws_s3_bucket" "static_assets" {
   bucket = "${var.company_prefix}-static-files-${random_id.random_static_assets_id.hex}"
 }
 
-resource "aws_s3_object " "loading_sandbox" {
+resource "aws_s3_object" "loading_sandbox" {
   bucket = aws_s3_bucket.static_assets.bucket
   key    = "loading_sandbox.html"
   source = "${path.module}/templates/loading_sandbox.html"
-
+  content_type = "text/html"
   etag = filemd5("${path.module}/templates/loading_sandbox.html")
+}
+
+resource "aws_s3_bucket_policy" "static_assets" {
+  bucket = aws_s3_bucket.static_assets.id
+
+  policy = jsonencode({
+        "Version": "2008-10-17",
+        "Id": "PolicyForCloudFrontPrivateContent",
+        "Statement": [
+            {
+                "Sid": "AllowCloudFrontServicePrincipal",
+                "Effect": "Allow",
+                "Principal": {
+                    "Service": "cloudfront.amazonaws.com"
+                },
+                "Action": "s3:GetObject",
+                "Resource": "${aws_s3_bucket.static_assets.arn}/*",
+                "Condition": {
+                    "StringEquals": {
+                      "AWS:SourceArn": aws_cloudfront_distribution.loading_sandbox.arn
+                    }
+                }
+            }
+        ]
+      })
+}
+
+resource "aws_s3_bucket" "cloudfront_access_logs" {
+  bucket = "${var.company_prefix}-cf-access-logs-${random_id.random_static_assets_id.hex}"
+}
+
+
+resource "aws_cloudfront_origin_access_control" "loading_sandbox_s3_oac" {
+  origin_access_control_origin_type = "s3"
+  name                              = "${var.company_prefix}_loading_sandbox_s3_oac"
+  signing_behavior                  = "always"
+  signing_protocol                  = "sigv4"
 }
 
 resource "aws_cloudfront_distribution" "loading_sandbox" {
   origin {
-    domain_name = aws_s3_bucket.static_assets.bucket_domain_name
+    domain_name = aws_s3_bucket.static_assets.bucket_regional_domain_name
+    origin_access_control_id = aws_cloudfront_origin_access_control.loading_sandbox_s3_oac.id
     origin_id   = "static-assets"
   }
 
@@ -30,19 +68,9 @@ resource "aws_cloudfront_distribution" "loading_sandbox" {
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "static-assets"
 
-    forwarded_values {
-      query_string = false
-
-      cookies {
-        forward = "none"
-      }
-    }
-
-    viewer_protocol_policy = "redirect-to-https"
-    min_ttl                = 0
-    default_ttl            = 3600
-    max_ttl                = 86400
     compress               = true
+    viewer_protocol_policy = "allow-all"
+    cache_policy_id        = "658327ea-f89d-4fab-a63d-7e88639e58f6"
   }
 
   viewer_certificate {
@@ -54,10 +82,15 @@ resource "aws_cloudfront_distribution" "loading_sandbox" {
       restriction_type = "none"
     }
   }
+
+  logging_config {
+    bucket = aws_s3_bucket.cloudfront_access_logs.bucket_domain_name
+    prefix = "${var.company_prefix}/loading_sandbox/"
+  }
 }
 
 resource "aws_cloudfront_origin_access_control" "static_assets" {
-  name                              = "static-assets"
+  name                              = "${var.company_prefix}-static-assets-ac"
   origin_access_control_origin_type = "s3"
   signing_behavior                  = "always"
   signing_protocol                  = "sigv4"
